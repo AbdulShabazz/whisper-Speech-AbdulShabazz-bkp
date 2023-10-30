392f3fe5e4663234641930cdd120a6fe5fe19983
AbdulShabazz <AbdulShabazz@users.noreply.github.com>
2023-10-30 03:28:08 -0400

    Update generate_log_mel_spectrogram.py

--- whisper/generate_log_mel_spectrogram.py
+++ whisper/generate_log_mel_spectrogram.py
@@ -1,12 +1,12 @@
 '''Generate log mel spectrogram from .WAV audio file'''
 import audio as whisper_audio
-import numpy as np
 import matplotlib.pyplot as plt
 import torch
+import numpy
 
 # returns a numpy array of the audio file of float32 type
-audio_file_np_array_float32 = whisper_audio.load_audio('combined_sine_waves.wav', 44100)
-
+audio_file_np_array_float32 = whisper_audio.load_audio("combined_sine_waves.wav", 44100)
+'''
 # returns a torch tensor that contains the log mel spectrogram of the audio file
 audio_file_log_mel_spectrogram_torch_tensor = whisper_audio.log_mel_spectrogram(audio_file_np_array_float32, n_mels=80, padding=0, device=None)
 
@@ -22,3 +22,4 @@ plt.ylabel("Mel-frequency bins")
 # Show Plot
 plt.tight_layout()
 plt.show()
+'''
